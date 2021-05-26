71b6e5be8308ac89101b7233303de6d8d4c860eb
Roi malka <roimalka10@gmail.com>
2021-05-26 23:16:33 +0300

    added functioning sidebar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,9 @@ import { IconContext } from "react-icons/lib";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  const handleClick = () => setShowMenu(!showMenu);
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <>
@@ -15,26 +17,38 @@ const Navbar = () => {
           <a className="logo" onClick={handleClick} href="/home">
             <img src="https://d33wubrfki0l68.cloudfront.net/7d67067382c65bc1d6deb566dc125e2091c377be/1d162/images/name-logo.jpeg" />
           </a>
+          <ul className="navbar-lg-item">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
           <div className="burger__icon" onClick={handleClick}>
-            {showMenu ? <FaTimes size={30} /> : <FaBars size={30} />}
-          </div>
-          <div className="navbar__items" onClick={handleClick} click={showMenu}>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/projects">Projects</a>
-              </li>
-              <li>
-                <a href="/about">About</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
+            {showMenu ? <FaTimes size={29} /> : <FaBars size={29} />}
           </div>
         </div>
+        <ul className="navbar-sm-item" hidden={!showMenu}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
       </IconContext.Provider>
     </>
   );
