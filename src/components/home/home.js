6984f235be6a2ac5622e9539d31f6ec08cc416de
Roi malka <roimalka10@gmail.com>
2021-05-28 21:31:59 +0300

    fixed media queries for navbar and home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -22,6 +22,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <ul className="icons"></ul>
     </section>
   );
 };
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -14,25 +14,27 @@ const Navbar = () => {
     <>
       <IconContext.Provider value={{ color: "white" }}>
         <div className="navbar">
-          <a className="logo" onClick={handleClick} href="/home">
-            <img src="https://d33wubrfki0l68.cloudfront.net/7d67067382c65bc1d6deb566dc125e2091c377be/1d162/images/name-logo.jpeg" />
-          </a>
-          <ul className="navbar-lg-item">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/projects">Projects</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-          <div className="burger-icon">
-            <FaBars size={28} onClick={handleClick} />
+          <div className="content">
+            <a className="logo" onClick={handleClick} href="/home">
+              <img src="https://d33wubrfki0l68.cloudfront.net/7d67067382c65bc1d6deb566dc125e2091c377be/1d162/images/name-logo.jpeg" />
+            </a>
+            <ul className="navbar-lg-item">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/projects">Projects</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+            <div className="burger-icon">
+              <FaBars size={28} onClick={handleClick} />
+            </div>
           </div>
         </div>
         <nav className={showMenu ? "nav-menu active" : "nav-menu"}>
