61c42b1274ba50f39c7578e07ae7e72eaed96e8c
Roi malka <roimalka10@gmail.com>
2021-06-10 17:14:55 +0300

    added a bit of fade on scroll to text

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./projects.scss";
+import Fade from "react-reveal/Fade";
 
 const Projects = () => {
   return (
     <section id="projects">
       <div className="p-content">
-        <div className="p-text">Projects</div>
+        <Fade top duration={700}>
+          <div className="p-text">Projects</div>
+        </Fade>
         <ul className="p-list">
           <li>
             <a
@@ -17,13 +20,15 @@ const Projects = () => {
               <div className="mask"></div>
               <div className="project-number">01</div>
               <div className="text-section">
-                <h3 className="project-title">Personal Portfolio</h3>
-                <h5 className="project-subtitle">
-                  React | Sass | Framer Motion
-                </h5>
-                <button className="the-button">
-                  <span className="button-text">See Code</span>
-                </button>
+                <Fade bottom duration={800}>
+                  <h3 className="project-title">Personal Portfolio</h3>
+                  <h5 className="project-subtitle">
+                    React | Sass | Framer Motion
+                  </h5>
+                  <button className="the-button">
+                    <span className="button-text">See Code</span>
+                  </button>
+                </Fade>
               </div>
             </a>
           </li>
@@ -37,13 +42,15 @@ const Projects = () => {
               <div className="mask2"></div>
               <div className="project-number2">02</div>
               <div className="text-section2">
-                <h3 className="project-title">Second Project</h3>
-                <h5 className="project-subtitle">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </h5>
-                <button className="the-button">
-                  <span className="button-text">See Code</span>
-                </button>
+                <Fade bottom duration={800}>
+                  <h3 className="project-title">Second Project</h3>
+                  <h5 className="project-subtitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </h5>
+                  <button className="the-button">
+                    <span className="button-text">See Code</span>
+                  </button>
+                </Fade>
               </div>
             </a>
           </li>
@@ -56,14 +63,17 @@ const Projects = () => {
             >
               <div className="mask3"></div>
               <div className="project-number">03</div>
+
               <div className="text-section">
-                <h3 className="project-title">Third Project</h3>
-                <h5 className="project-subtitle">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </h5>
-                <button className="the-button">
-                  <span className="button-text">See Code</span>
-                </button>
+                <Fade bottom duration={800}>
+                  <h3 className="project-title">Third Project</h3>
+                  <h5 className="project-subtitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                  </h5>
+                  <button className="the-button">
+                    <span className="button-text">See Code</span>
+                  </button>
+                </Fade>
               </div>
             </a>
           </li>
