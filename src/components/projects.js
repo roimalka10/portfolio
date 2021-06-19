71a9527028c2e3092110b3a554097d190e64d02e
Roi malka <roimalka10@gmail.com>
2021-06-19 17:09:57 +0300

    changed navbar padding a bit

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
-
+import React, { useEffect, useRef, createRef, useState } from "react";
+import hoverEffect from "hover-effect";
 import { motion, useTransform, useViewportScroll } from "framer-motion";
 import Fade from "react-reveal/Fade";
+import { projects } from "../data/data";
 
 const Projects = () => {
   const { scrollYProgress } = useViewportScroll();
@@ -11,89 +12,58 @@ const Projects = () => {
   const x2 = useTransform(scrollYProgress, [0.9, 0.1], [500, 0]);
 
   const [size, setSize] = useState(window.innerHeight);
-  return (
-    <section id="projects">
-      <div className="p-content">
-        <motion.h1 style={{ x: size > 600 ? x : x2 }} className="p-text">
-          {" "}
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-          <span>projects </span>
-        </motion.h1>
-        <ul className="p-list">
-          <li>
-            <a
-              href="https://github.com/roimalka10/portfolio"
-              target="_blank"
-              id="first"
-              className="project1"
-            >
-              <div className="mask"></div>
-              <div className="project-number">01</div>
-              <div className="text-section">
-                <Fade bottom duration={800}>
-                  <h3 className="project-title">Personal Portfolio</h3>
-                  <h5 className="project-subtitle">
-                    React | Sass | Framer Motion
-                  </h5>
-                  <button className="the-button">
-                    <span className="button-text">See Code</span>
-                  </button>
-                </Fade>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/roimalka10/portfolio"
-              target="_blank"
-              id="first"
-              className="project2"
-            >
-              <div className="mask2"></div>
-              <div className="project-number2">02</div>
-              <div className="text-section2">
-                <Fade bottom duration={800}>
-                  <h3 className="project-title">Second Project</h3>
-                  <h5 className="project-subtitle">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                  </h5>
-                  <button className="the-button">
-                    <span className="button-text">See Code</span>
-                  </button>
-                </Fade>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/roimalka10/portfolio"
-              target="_blank"
-              id="first"
-              className="project3"
-            >
-              <div className="mask3"></div>
-              <div className="project-number">03</div>
 
-              <div className="text-section">
-                <Fade bottom duration={800}>
-                  <h3 className="project-title">Third Project</h3>
-                  <h5 className="project-subtitle">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                  </h5>
-                  <button className="the-button">
-                    <span className="button-text">See Code</span>
-                  </button>
-                </Fade>
+  let wrappers = useRef({});
+
+  projects.map((project) => (wrappers.current[project.id - 1] = createRef()));
+
+  useEffect(() => {
+    projects.map(
+      (project) =>
+        new hoverEffect({
+          parent: wrappers.current[project.id - 1].current,
+          intensity: 0.4,
+          imagesRatio: 1080 / 1920,
+          image1: project.img,
+          image2: project.img2,
+          displacementImage: project.distort,
+        })
+    );
+  }, []);
+  return (
+    <section className="projects">
+      <motion.h1 style={{ x: size > 600 ? x : x2 }} className="p-text">
+        {" "}
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+        <span>projects </span>
+      </motion.h1>
+      <div className="list container">
+        {projects.map((project) => (
+          <div key={project.id} className={`project${project.id}`}>
+            <div className="title">
+              <div className="title-main">
+                <h3>0{project.id}</h3>
+                <h3>{project.title}</h3>
               </div>
+              <p>{project.type}</p>
+              <button className="btn">
+                <span>See Code</span>
+              </button>
+            </div>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <div
+                className="wrapper"
+                ref={wrappers.current[project.id - 1]}
+              ></div>
             </a>
-          </li>
-        </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
