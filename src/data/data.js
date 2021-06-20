b063b8ed5f0915c4103e56a75a0c620e230e2290
Roi malka <roimalka10@gmail.com>
2021-06-20 16:40:43 +0300

    added different distortion pictures

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,17 @@
 import portfolio from "../images/portfolio1.jpg";
 import portfolio2 from "../images/portfolio2.jpg";
 import trainer from "../images/training.jpg";
+import trainer2 from "../images/project3.jpg";
 import nasaApi from "../images/stars.jpg";
 import nasa2 from "../images/nasa2.jpg";
 import distorted from "../images/distorted.jpg";
+import distorted2 from "../images/distorted2.jpeg";
+import distorted3 from "../images/distorted3.jpg";
 
 export const projects = [
   {
     id: 1,
-    title: "Personal Portfolio",
+    title: "Portfolio",
     img: portfolio,
     img2: portfolio2,
     distort: distorted,
@@ -19,8 +22,8 @@ export const projects = [
     id: 2,
     title: "Personal Trainer Studio",
     img: trainer,
-    img2: portfolio,
-    distort: distorted,
+    img2: trainer2,
+    distort: distorted2,
     type: "React / Sass / Framer Motion",
     link: "https://github.com/roimalka10/Fitness-website",
   },
@@ -29,7 +32,7 @@ export const projects = [
     title: "Nasa API",
     img: nasa2,
     img2: nasaApi,
-    distort: distorted,
+    distort: distorted3,
     type: "Frontend Web Development",
     link: "https://github.com/roimalka10/Fitness-website",
   },
